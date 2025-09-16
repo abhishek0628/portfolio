8a1693ebf734e3f04afeb619c3e840d15205ffe5
Abhishek Kumar <151183785+abhishek0628@users.noreply.github.com>
2025-09-17 01:25:26 +0530

    changes made

--- script.js
+++
@@ -1,9 +0,0 @@
-// function showmeassage()
-// {
-//     alert("hello from external javascript");
-// }
-// script.js
-// function showMessage() {
-//     alert("Hello from external JavaScript!");
-//   }
-  
