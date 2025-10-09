76420e788138ba59903eb2f7ee6c3323992580d3
Abhishek0628 <abhijnv124@gmail.com>
2025-10-10 04:59:44 +0530

    changes made

---
+++ Script/blog.js
@@ -0,0 +1,22 @@
+// const os=document.getElementById("os");
+// os.addEventListener("click",()=>{
+//     window.location.href="OS.html";
+// });
+const pages = {
+    os: "./Courses/os.html",
+    dbms: "./Courses/dbms.html",
+    ca: "./Courses/ca.html",
+    cn: "./Courses/cn.html",
+    oops: "./Courses/oops.html",
+    cd: "./Courses/compiler.html"
+};
+
+// Attach click listeners to all buttons
+Object.keys(pages).forEach(id => {
+    const btn = document.getElementById(id);
+    if (btn) {
+        btn.addEventListener("click", () => {
+            window.location.href = pages[id];
+        });
+    }
+});
