81f95fb8c43e59fcc205e5c0ad6d7f006d7ebb39
Abhishek0628 <abhijnv124@gmail.com>
2026-03-21 03:19:35 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
 
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
-import Navbar from "./Components/Navbar"
-import Home from "./Pages/Home"
-import Projects from "./Pages/Projects"
-import Contacts from "./Pages/Contacts"
-import About from "./Pages/About"
-import Blog from "./Pages/Blog"
-import Courses from "./Pages/Courses"
+// import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+// import Navbar from "./Components/Navbar"
+// import Home from "./Pages/Home"
+// import Projects from "./Pages/Projects"
+// import Contacts from "./Pages/Contacts"
+// import About from "./Pages/About"
+// import Blog from "./Pages/Blog"
+// import Courses from "./Pages/Courses"
 
-function App(){
-  return(
+// function App(){
+//   return(
+//     <Router>
+//       <Navbar/>
+//       <Routes>
+//         <Route path="/" element={<Home/>}/>
+//         <Route path="/projects" element={<Projects/>}/>
+//         <Route path="/contacts" element={<Contacts/>}/>
+//         <Route path="/About" element={<About/>}/>
+//         <Route path="/Blog" element={<Blog/>}/>
+//         <Route path="/Courses" element={<Courses/>}/>
+
+//       </Routes>
+//     </Router>
+//   )
+// }
+// export default App
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import Projects from "./Pages/Projects";
+import Contacts from "./Pages/Contacts";
+import About from "./Pages/About";
+import Blog from "./Pages/Blog";
+import Courses from "./Pages/Courses";
+
+function App() {
+  return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/projects" element={<Projects/>}/>
-        <Route path="/contacts" element={<Contacts/>}/>
-        <Route path="/About" element={<About/>}/>
-        <Route path="/Blog" element={<Blog/>}/>
-        <Route path="/Courses" element={<Courses/>}/>
-
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Courses" element={<Courses />} />
       </Routes>
     </Router>
-  )
+  );
 }
-export default App
+
+export default App;
